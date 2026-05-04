93cdfc5935bf889a8beeacffce80e41de027d7ef
Kops147 <kops147@gmail.com>
2026-05-04 14:48:28 +0200

    feat: add 38 new product photos to manifest, wire catalog to real images

--- js/catalog.js
+++ js/catalog.js
@@ -6,21 +6,21 @@
    ============================================= */
 
 export const LOCAL_CATALOG = [
-  { stock_code: "1125", name: "Short Rib",            price: 130.00, unit: "per kg",    category: "Beef",     image: "assets/img/food/meat_on_braai.jpg" },
-  { stock_code: "1126", name: "Beef Stew",             price: 100.00, unit: "per kg",    category: "Beef",     image: "assets/img/food/meat_on_braai.jpg" },
-  { stock_code: "1124", name: "Brisket",               price: 130.00, unit: "per kg",    category: "Beef",     image: "assets/img/food/1124.jpg" },
-  { stock_code: "137",  name: "Bulk Mince",             price:  80.00, unit: "per kg",    category: "Beef",     image: "assets/img/food/meat_on_braai.jpg" },
-  { stock_code: "1123", name: "Chuck",                 price: 130.00, unit: "per kg",    category: "Beef",     image: "assets/img/food/1123.jpg" },
-  { stock_code: "1166", name: "Frozen Chicken Hearts", price:  35.00, unit: "each",      category: "Chicken",  image: "assets/img/food/meat_on_braai.jpg" },
-  { stock_code: "37",   name: "Lamb Tripe",             price:  30.00, unit: "per kg",    category: "Lamb",     image: "assets/img/food/meat_on_braai.jpg" },
-  { stock_code: "291",  name: "Lamb Chops",             price: 140.00, unit: "per kg",    category: "Lamb",     image: "assets/img/food/291.jpg" },
-  { stock_code: "1179", name: "Lamb Stew",             price: 140.00, unit: "per kg",    category: "Lamb",     image: "assets/img/food/1179.jpg" },
-  { stock_code: "130",  name: "Black Lentils",          price:  10.00, unit: "per 200g",  category: "Other",    image: "assets/img/food/meat_on_braai.jpg" },
-  { stock_code: "124",  name: "Pink Lentils",           price:  15.00, unit: "per 200g",  category: "Other",    image: "assets/img/food/meat_on_braai.jpg" },
-  { stock_code: "1206", name: "Boerewors",             price:  95.99, unit: "per kg",    category: "Sausages", image: "assets/img/food/1206.jpg" },
-  { stock_code: "1112", name: "Chilli Bites",          price: 350.00, unit: "each",      category: "Sides",    image: "assets/img/food/ChilliBytes.jpg" },
-  { stock_code: "atchar",         name: "Atchar",        price:   6.00, unit: "each",      category: "Sides",    image: "assets/img/food/meat_on_braai.jpg" },
-  { stock_code: "curry-chillies", name: "Curry Chillies",price:  10.00, unit: "each",      category: "Sides",    image: "assets/img/food/meat_on_braai.jpg" },
+  { stock_code: "1125", name: "Short Rib",            price: 130.00, unit: "per kg",    category: "Beef",     image: "assets/img/food/1125ShortRib R130 per kilo.jpeg" },
+  { stock_code: "1126", name: "Beef Stew",             price: 100.00, unit: "per kg",    category: "Beef",     image: "assets/img/food/1126 BeefStew R100 per kilo.jpeg" },
+  { stock_code: "1124", name: "Brisket",               price: 130.00, unit: "per kg",    category: "Beef",     image: "assets/img/food/1124 Brisket R130 per kg.jpeg" },
+  { stock_code: "137",  name: "Bulk Mince",             price:  80.00, unit: "per kg",    category: "Beef",     image: "assets/img/food/137  BulkMince R80perKilo.jpeg" },
+  { stock_code: "1123", name: "Chuck",                 price: 130.00, unit: "per kg",    category: "Beef",     image: "assets/img/food/1123 Chuck R130 per kilo.jpeg" },
+  { stock_code: "1166", name: "Frozen Chicken Hearts", price:  35.00, unit: "each",      category: "Chicken",  image: "assets/img/food/1166 frozen chicken hearts R35.jpeg" },
+  { stock_code: "37",   name: "Lamb Tripe",             price:  30.00, unit: "per kg",    category: "Lamb",     image: "assets/img/food/37 Lamb tribe R30 per kg.jpeg" },
+  { stock_code: "291",  name: "Lamb Chops",             price: 140.00, unit: "per kg",    category: "Lamb",     image: "assets/img/food/291 LambChops R140 per kilo.jpeg" },
+  { stock_code: "1179", name: "Lamb Stew",             price: 140.00, unit: "per kg",    category: "Lamb",     image: "assets/img/food/1179 LambStew R140perKilo.jpeg" },
+  { stock_code: "130",  name: "Black Lentils",          price:  10.00, unit: "per 200g",  category: "Other",    image: "assets/img/food/130 BlackLintels R10 for 200grams.jpeg" },
+  { stock_code: "124",  name: "Pink Lentils",           price:  15.00, unit: "per 200g",  category: "Other",    image: "assets/img/food/124 PinkLintels R15 for 200gram.jpeg" },
+  { stock_code: "1206", name: "Boerewors",             price:  95.99, unit: "per kg",    category: "Sausages", image: "assets/img/food/1206 Boerewors  R95.99 per kilo.jpeg" },
+  { stock_code: "1112", name: "Chilli Bites",          price: 350.00, unit: "each",      category: "Sides",    image: "assets/img/food/1112 Chilli Bytes R350.jpeg" },
+  { stock_code: "atchar",         name: "Atchar",        price:   6.00, unit: "each",      category: "Sides",    image: "assets/img/food/Sides-Atchar R6.jpeg" },
+  { stock_code: "curry-chillies", name: "Curry Chillies",price:  10.00, unit: "each",      category: "Sides",    image: "assets/img/food/Sides-CurryChillies R10.jpeg" },
 ];
 
 const COOKED_CATEGORIES = new Set(['Sides', 'Other']);
